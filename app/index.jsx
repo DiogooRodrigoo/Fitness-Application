6ca05ebd088653b0582943e02cbefb7533c0cd54
Diogo Rodrigo Pedreira Galvão <diogo.rgalvao@outlook.com>
2024-06-07 19:24:31 -0300

    feature: Development of the home screen, styling with Tailwind, and addition of a library for animation effects

--- app/index.jsx
+++ app/index.jsx
@@ -1,14 +1,67 @@
-import { View, Text } from "react-native";
+import { View, Text, Image, TouchableOpacity } from "react-native";
+import React from "react";
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
 } from "react-native-responsive-screen";
-import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { LinearGradient } from "expo-linear-gradient";
+import Animated, { FadeIn, FadeOut, FadeInDown } from "react-native-reanimated";
+import { useRoute } from "@react-navigation/native";
 
-export default function index() {
+export default function Index() {
+  const router = useRoute();
+
   return (
-    <View className="flex-1 items-center justify-center">
-      <Text>This is home page</Text>
+    <View className="flex-1 flex justify-end">
+      <StatusBar style="light" />
+      <Image
+        className="h-full w-full absolute"
+        source={require("../assets/images/welcome.jpg")}
+      />
+
+      <LinearGradient
+        colors={["transparent", "#18181b"]}
+        style={{ width: wp(100), height: hp(70) }}
+        start={{ x: 0.5, y: 0 }}
+        end={{ x: 0.5, y: 0.8 }}
+        className="flex justify-end pb-12 space-y-8"
+      >
+        {/* Welcome Text */}
+        <Animated.View
+          entering={FadeInDown.delay(100).springify()}
+          className="flex items-center"
+        >
+          <Text
+            style={{ fontSize: hp(3.5) }}
+            className="text-white font-bold tracking-wide"
+          >
+            Melhores <Text className="text-rose-500">Treinos</Text>
+          </Text>
+          <Text
+            style={{ fontSize: hp(3.5) }}
+            className="text-white font-bold tracking-wide"
+          >
+            Para você
+          </Text>
+        </Animated.View>
+
+        {/* Start Button */}
+        <Animated.View entering={FadeInDown.delay(300).springify()}>
+          <TouchableOpacity
+            onPress={() => router.push("home")}
+            style={{ height: hp(7), width: wp(80) }}
+            className="bg-rose-600 flex, items-center justify-center mx-auto rounded-full border-[2px] border-neutral-200"
+          >
+            <Text
+              style={{ fontSize: hp(2.5) }}
+              className="text-white font-bold tracking-widest"
+            >
+              Iniciar
+            </Text>
+          </TouchableOpacity>
+        </Animated.View>
+      </LinearGradient>
     </View>
   );
 }
